192b36b45c3901b684f414e7d1ce96a99aeaf931
Victor Oviedo <victor.oviedo.a87@gmail.com>
2024-04-28 12:34:24 -0300

    creando PatientController

---
+++ src/main/java/com/vao/agenda/controller/PatientController.java
@@ -0,0 +1,19 @@
+package com.vao.agenda.controller;
+
+import com.vao.agenda.entity.Patient;
+import com.vao.agenda.repository.IPatientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RequestMapping("/patient")
+@RestController
+public class PatientController {
+
+    @Autowired
+    private IPatientRepository iPatientRepository;
+
+
+}
