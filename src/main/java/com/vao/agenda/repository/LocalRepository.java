d2d4e0d51a04ec920fb6eea62ac3a1ac235cba5e
Victor Oviedo <victor.oviedo.a87@gmail.com>
2024-05-25 17:03:05 -0300

    agregando repository Local y Trataiento

---
+++ src/main/java/com/vao/agenda/repository/LocalRepository.java
@@ -0,0 +1,7 @@
+package com.vao.agenda.repository;
+
+import com.vao.agenda.entity.Local;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LocalRepository extends JpaRepository<Local, Long> {
+}
---
+++ src/main/java/com/vao/agenda/repository/TratamientoRepository.java
@@ -0,0 +1,7 @@
+package com.vao.agenda.repository;
+
+import com.vao.agenda.entity.Tratamiento;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TratamientoRepository extends JpaRepository<Tratamiento, Long> {
+}
